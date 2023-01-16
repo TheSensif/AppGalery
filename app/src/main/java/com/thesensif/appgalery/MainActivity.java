bff067827a214c222ddd420cb90f06720373e2a6
TheSensif <sergisimon7@gmail.com>
2023-01-16 17:43:42 +0100

    finish thumbnail

--- app/src/main/java/com/thesensif/appgalery/MainActivity.java
+++ app/src/main/java/com/thesensif/appgalery/MainActivity.java
@@ -4,23 +4,27 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView imageView;
     public static int RC_PHOTO_PICKER = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.img);
 
         ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -31,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
                             // There are no request codes
                             Intent data = result.getData();
                             Uri uri = data.getData();
-                            ImageView imageView = findViewById(R.id.img);
                             imageView.setImageURI(uri);
                         }
                     }
@@ -50,10 +53,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ActivityResultLauncher<Intent> cameraActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Intent data = result.getData();
+                        Bundle extras = data.getExtras();
+                        Bitmap imageBitmap = (Bitmap) extras.get("data");
+                        imageView.setImageBitmap(imageBitmap);
+
+                    }
+                });
+
         Button bCamera = findViewById(R.id.bCamera);
         bCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //Launch activity to get result
+                cameraActivityResultLauncher.launch(intent);
 
             }
         });
